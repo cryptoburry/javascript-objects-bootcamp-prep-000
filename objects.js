00c848d4147250f984b57ee3c2da702a372adc9e
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-03-22 18:11:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,17 @@
 var playlist = {
-  SherylCrow: "All I wanna Do"}
+  SherylCrow: "All I wanna Do",
+  ACDC: "Back In Black"
+};
+
+//limitations on playlist: using playlist{} we can only look up song names (values) by artist (keys)
+
+function updatePlaylist(playlist,artist,song){
+  playlist.artist = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist,artist){
+  delete playlist.artist;
+  return playlist;
+}
+
